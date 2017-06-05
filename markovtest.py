f781ad2a3fb7498ad11008a3142a061ce5893478
Ellest <syunelli@gmail.com>
2017-06-04 22:19:11 -0500

    deleted files

--- markovtest.py
+++
@@ -1,9 +0,0 @@
-import markovify
-
-with open("data/ed_sheeran_give_me_love.txt") as f:
-	text = f.read()
-
-text_model = markovify.Text(text)
-
-for i in range(50):
-    print(text_model.make_short_sentence(140))
